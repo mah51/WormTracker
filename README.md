616bca304e4d388b812508b50521a8f0dbf3565b
mah51 <mah51@kent.ac.uk>
2021-02-13 03:45:19 +0000

    Changing readme

--- README.md
+++ README.md
@@ -1,70 +1,18 @@
-# Getting Started with Create React App
+# WormTracker v1.0
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+A website built using react that allows the user to load an image and overlay a grid for counting worm tracks on an agar plate.
 
-In the project directory, you can run:
+### How to use:
 
-### `yarn start`
+1. Make sure image is in a web useable format (jpg, jpeg, png etc. (not TIFF)) (Most websites can convert these or you can go into paint and save as png)
+2. Upload picture to some public webserver, in my examples i send my pictures on a discord channel and copy link so the website has access to them.
+3. Set X and Y start points with the sliders, then set height and width to range in the grid
+4. Click squares with worm tracks in, they will change to green. Note, a count of squares is displayed on the right.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+###//TODO:
 
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- Add ability to zoom :/
+- Add copy settings to clipboard button.
+- General display fixes. 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useEffect, useState} from 'react';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
 
 function App() {
+
+  const [imageURL, setImageURL] = useState('https://cdn.discordapp.com/attachments/698573610012639302/809945399740727307/dsBuffer.bmp.png')
+  const [table, setTable] = useState([]);
+  const [count, setCount] = useState(0);
+  const [xGrid, setXGrid] = useState(0);
+  const [yGrid, setYGrid] = useState(0);
+  const [heightGrid, setHeightGrid] = useState(50);
+  const [widthGrid, setWidthGrid] = useState(50);
+  const [imageSize, setImageSize] = useState(false)
+
+  useEffect(() => {
+    if (imageURL && table.length === 0) {
+      const tables = [];
+      for ( let i = 0; i < 11; i++) {
+        const rowArray = [];
+        for (let j = 0; j < 11; j++) {
+          rowArray.push({selected: false, cellNumber: j})
+        }
+        tables.push({rowNumber: i, cells: rowArray})
+      }
+      setTable(tables)
+    }
+  },[table])
+
+  function handleGridXChange(event, value) {
+    setXGrid(value)
+  }
+
+  function handleGridYChange(event, value) {
+    setYGrid(value)
+  }
+
+  function handleGridHeightChange(event, value) {
+    setHeightGrid(value)
+  }
+
+  function handleGridWidthChange(event, value) {
+    setWidthGrid(value)
+  }
+
+  function handleGridClick(rowNumber, cellNumber) {
+    table[rowNumber].cells[cellNumber].selected = !table[rowNumber].cells[cellNumber].selected;
+    setTable([...table]);
+    const increment = table[rowNumber].cells[cellNumber].selected ? 1 : -1;
+    setCount(count + increment  );
+  }
+
+  function onImgLoad({target: img}) {
+    console.log(img.offsetHeight)
+    setImageSize([img.offsetWidth, img.offsetHeight])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="container">
+        <img
+          className={"img"}
+          src={imageURL}
+          onLoad={onImgLoad}
+          alt="Image of worm tracks hopefully :)"
+        />
+        <table
+          className={"table"}
+          style={
+            {
+              height: (heightGrid * 2).toString() + 'vh',
+              width: (widthGrid * 2).toString() + 'vh',
+              top: (yGrid.toString() * 4) + 'px',
+              left: (xGrid.toString()* 4) + 'px'
+            }
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {
+            table.map(row => {
+              return (
+                <tr key={row.rowNumber}>
+                  {
+                    row.cells.map(cell => {
+                      return <td key={`${row.rowNumber}:${cell.cellNumber}`} style={{ opacity: '10%', backgroundColor: cell.selected ? 'green' : 'red'}} onClick={() => handleGridClick(row.rowNumber, cell.cellNumber)} />
+                    })
+                  }
+                </tr>
+              )
+
+            })
+          }
+
+        </table>
+
+      </div>
+      <div className="formcontainer">
+        <form >
+          <p>Number of grids: {count}</p>
+          <p>Image link:</p>
+          <input type="text" placeholder={imageURL} onChange={(e) => setImageURL(e.target.value) }/>
+          <button onClick={(e) => {
+            setTable([]);
+            setCount(0);
+            setXGrid(0);
+            setYGrid(0);
+            setWidthGrid(50);
+            setWidthGrid(50);
+            e.preventDefault();
+          }}>Reset Grid</button>
+          <div className="gridCoordContainer">
+            <p>x</p>
+            <Slider value={xGrid} onChange={handleGridXChange} aria-labelledby="continuous-slider" />
+            <p>y</p>
+            <Slider value={yGrid} onChange={handleGridYChange} aria-labelledby="continuous-slider" />
+
+          </div>
+          <div className="gridLengthHeight">
+            <p>height</p>
+            <Slider value={heightGrid} onChange={handleGridHeightChange} min={10} max={90}  aria-labelledby="continuous-slider" />
+            <p>width</p>
+            <Slider value={widthGrid} onChange={handleGridWidthChange} min={10} max={90}  aria-labelledby="continuous-slider" />
+          </div>
+        </form>
+      </div>
+    </>
+
+  )
 }
 
 export default App;
+
